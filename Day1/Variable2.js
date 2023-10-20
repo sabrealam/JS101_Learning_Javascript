4afd1ebf0c5028f18693cf6e4b76f746f4d51e63
Sabre Alam <asabre5073@gmail.com>
2023-10-20 23:28:48 +0545

    Added V2 Content

---
+++ Day1/Variable2.js
@@ -0,0 +1,37 @@
+// First Method ---------------------------
+
+// var name = "sabre alam";
+// console.log(name);
+
+// var name = "Mr. Raj Husain";
+// console.log(name);
+
+// var name = "Mrs. Hemja Khatoon";
+// console.log(name);
+
+
+// Second Method  ----------------------------
+
+// var name;
+
+// name = "sabre alam";
+// console.log(name);
+
+// name = "Mr. Raj Husain";
+// console.log(name);
+
+// name = "Mrs. Hemja Khatoon";
+// console.log(name);
+
+// Third Method  ---------------------------
+
+var name;
+
+name = "Sabre Alam"
+console.log(name);
+
+name = "Mr. Raj Husain";
+console.log(name);
+
+name = "Mrs. Hemja Khatoon";
+console.log(name);
