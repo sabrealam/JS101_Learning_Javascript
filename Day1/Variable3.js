519d0d9a38cb3fafe466100df718f809d1dff24d
Sabre Alam <asabre5073@gmail.com>
2023-10-20 23:34:16 +0545

    Added V3 And V4

---
+++ Day1/Variable3.js
@@ -0,0 +1,4 @@
+var name = "Sabre Alam";
+var age  = 21;
+console.log(name);
+console.log(age);
---
+++ Day1/Variable4.js
@@ -0,0 +1,18 @@
+var name  = 'Sabre alam';
+var school = 'Masai School';
+var Grade = 'A+';
+var section = 'a';
+var roll_no = 123456789;
+var math = 82;
+var english = 77;
+var science = 73;
+console.log(`${name} From ${school}`);
+console.log("Scored In Math" + " " + math);
+console.log("Scored In English" + " " + english);
+console.log("Scored In Science" + " " + science);
+a = math + english + science;
+console.log('Total' + 'Score '+ '' + a);
+
+
+// Pecentage
+console.log('Percentage' + ' ' + (a/300)*100 + "%");
